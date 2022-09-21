766f26a280b926c62557e56559b5efdf7e02f881
Ferrel John Fernando <ferreluchiha@gmail.com>
2022-09-21 15:27:49 +0700

    refactor: Upgrade dependencies and support to node 16

--- src/index.js
+++ src/index.js
@@ -46,7 +46,8 @@ class Optimize {
       this.serverless.service.provider.runtime === 'nodejs8.10' ||
       this.serverless.service.provider.runtime === 'nodejs10.x' ||
       this.serverless.service.provider.runtime === 'nodejs12.x' ||
-      this.serverless.service.provider.runtime === 'nodejs14.x')
+      this.serverless.service.provider.runtime === 'nodejs14.x' ||
+      this.serverless.service.provider.runtime === 'nodejs16.x')
 
     /** AWS provider and valid runtime check */
     if (validRunTime) {
